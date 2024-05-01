7043a5c5e739ef9d90dec88ceb854da3b81f72a9
Kaique Leonardo <kaiqueleonardo18@gmail.com>
2024-04-30 21:43:33 -0300

    Adicionando CORS e configuracoes swagger

--- MayTheFourth/MayTheFourth/Program.cs
+++ MayTheFourth/MayTheFourth/Program.cs
@@ -6,13 +6,39 @@ using MayTheFourth.Interfaces.Services;
 using MayTheFourth.Mappings.Filme;
 using MayTheFourth.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("RebelTech", builder =>
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MayTheFourth API - RebelTech",
+        Version = "v1",
+        Description = "API para consulta de dados do universo de Star Wars",
+        Contact = new OpenApiContact
+        {
+            Url = new Uri("https://github.com/nathalialasagna/desafio-balta-may-the-fourth-backend")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License"
+        }
+    });
+});
+
+builder.Services.ConfigureSwaggerGen(options => options.CustomSchemaIds(x => x.FullName));
 builder.Services.AddCarter();
 
 ConfigureServices(builder);
@@ -39,7 +65,6 @@ void ConfigureServices(WebApplicationBuilder builder)
     builder.Services.AddScoped<IPersonagemRepository, PersonagemRepository>();
     builder.Services.AddScoped<IPlanetaRepository, PlanetaRepository>();
     builder.Services.AddScoped<IVeiculoRepository, VeiculoRepository>();
-
 }
 
 void ConfigureApiServices(WebApplication webApplication)
